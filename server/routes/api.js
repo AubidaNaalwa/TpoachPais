740d9366ed4702c29806f30a057e7c15fd32d753
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2021-02-18 18:51:09 +0200

    WTF stop editiing server files

--- server/routes/api.js
+++ server/routes/api.js
@@ -14,78 +14,78 @@ router.get('/courses/:id?', (req, res) => {
             else
                 res.send({ course, status: 200 });
         })
-    }
+    }})
 
 
 
-router.post('/experiment', (req, res) => {
-    const experiment = new Experiments(req.body);
-    experiment.save();
-    res.end();
-});
-
-router.get('/experiments', (req, res) => {
-    Experiments.find({}, function (err, data) {
-        if (err)
-            res.send({ err, status: 400 });
-        else
-            res.send({ experiments: data, status: 200 });
+    router.post('/experiment', (req, res) => {
+        const experiment = new Experiments(req.body);
+        experiment.save();
+        res.end();
     });
-});
 
-router.get('/courses', (req, res) => {
-
-    Courses.find({}, function (err, data) {
-        if (err)
-            res.send({ err, status: 400 });
-        else
-            res.send({ courses: data, status: 200 });
+    router.get('/experiments', (req, res) => {
+        Experiments.find({}, function (err, data) {
+            if (err)
+                res.send({ err, status: 400 });
+            else
+                res.send({ experiments: data, status: 200 });
+        });
     });
-});
 
-router.post('/course', (req, res) => {
-    const course = new Courses(req.body);
-    course.save();
-    res.end();
-});
+    router.get('/courses', (req, res) => {
 
-router.get('/events', (req, res) => {
-    Events.find({}, function (err, data) {
-        if (err)
-            res.send({ err, status: 400 });
-        else
-            res.send({ events: data, status: 200 });
+        Courses.find({}, function (err, data) {
+            if (err)
+                res.send({ err, status: 400 });
+            else
+                res.send({ courses: data, status: 200 });
+        });
     });
-});
 
-router.post('/event', (req, res) => {
-    const event = new Events(req.body);
-    event.save();
-    res.end();
-});
-
-router.get('/images/:id', (req, res) => {
-    Images.find({ category: req.params.id }, function (err, data) {
-        if (err)
-            res.send({ err, status: 400 });
-        else
-            res.send({ images: data, status: 200 });
+    router.post('/course', (req, res) => {
+        const course = new Courses(req.body);
+        course.save();
+        res.end();
     });
-});
 
-router.post('/image', (req, res) => {
-    const image = new Images(req.body);
-    image.save();
-    res.end();
-});
+    router.get('/events', (req, res) => {
+        Events.find({}, function (err, data) {
+            if (err)
+                res.send({ err, status: 400 });
+            else
+                res.send({ events: data, status: 200 });
+        });
+    });
 
-router.get('/imagesCategory', async (req, res) => {
-    const results = await Images.aggregate().group(
-        {
-            _id: '$category'
-        }
-    )
-    res.send({ categories: results });
-});
+    router.post('/event', (req, res) => {
+        const event = new Events(req.body);
+        event.save();
+        res.end();
+    });
 
-module.exports = router;
+    router.get('/images/:id', (req, res) => {
+        Images.find({ category: req.params.id }, function (err, data) {
+            if (err)
+                res.send({ err, status: 400 });
+            else
+                res.send({ images: data, status: 200 });
+        });
+    });
+
+    router.post('/image', (req, res) => {
+        const image = new Images(req.body);
+        image.save();
+        res.end();
+    });
+
+    router.get('/imagesCategory', async (req, res) => {
+        const results = await Images.aggregate().group(
+            {
+                _id: '$category'
+            }
+        )
+        res.send({ categories: results });
+    });
+
+    module.exports = router;
