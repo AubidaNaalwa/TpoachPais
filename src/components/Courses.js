62598052ed012b3c87e1ceb1943e740e2df7cb52
Amro97 <57412900+Amro97@users.noreply.github.com>
2021-02-20 15:17:12 +0200

    bug fixes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -2,30 +2,31 @@ import React, { useEffect } from 'react';
 import CourseCard from './courseCard';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import '../styles/AubidaStyle.css'
 const axios = require('axios');
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1
-    },
-    paper: {
-        height: 140,
-        width: 100
-    },
-    control: {
-        padding: theme.spacing(2)
-    },
-    container: {
-        textAlign: 'center',
-        marginTop: '5%'
-    }
-}));
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         flexGrow: 1
+//     },
+//     paper: {
+
+//         width: "100%"
+//     },
+//     control: {
+//         padding: theme.spacing(2)
+//     },
+//     container: {
+//         textAlign: 'center',
+//         marginTop: '5%'
+//     }
+// }));
 
 export default function Courses(props) {
     //const [spacing, setSpacing] = React.useState(2);
     const [courses, setCourses] = React.useState([]),
-    classes = useStyles(),
-    func = props.setCourse;
+        // classes = useStyles(),
+        func = props.setCourse;
 
     // const handleChange = (event) => {
     //     setSpacing(Number(event.target.value));
@@ -40,20 +41,14 @@ export default function Courses(props) {
     useEffect(() => { fetchCourses() }, [])
 
     return (
-        <div className="content">
-            <div className={classes.container}>
-                <Grid container className={classes.root} spacing={2}>
-                    <Grid item xs={12}>
-                        <Grid container spacing={10}>
-                            {courses.map((value) => (
-                                <Grid key={value._id} item>
-                                    <CourseCard setCourse={func} course={value}/>
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </div>
+
+        <div className="AubidaStyle">
+            {
+                courses.map((value) => (
+                    <CourseCard setCourse={func} course={value} />
+                ))
+            }
         </div>
+
     );
 }
