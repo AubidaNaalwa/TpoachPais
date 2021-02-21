001c17078a770c56337699410021fe5f9829fcfa
Amro97 <57412900+Amro97@users.noreply.github.com>
2021-02-21 17:06:59 +0200

    hotfix

--- src/components/courseInfo.js
+++ src/components/courseInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect  } from 'react-router-dom';
+import Moment from 'react-moment';
 
 export default function CourseInfo(props) {
     const item = props.cInfo;
@@ -10,11 +11,11 @@ export default function CourseInfo(props) {
 
     return (
         <div style={{textAlign: 'right', marginRight: '20%'}}>
-            <h1>{item.name}</h1>
+            <h1 style={{textAlign: 'right'}}>{item.name}</h1>
             <img src={item.img} alt='img' style={{width:'80%', height:'75%'}}></img>
             <p>{item.longDescription}</p>
             <p>{item.availabaility}</p>
-            <p>{item.date}</p>
+            <p>{<Moment date={item.date} format="DD/MM/YYYY"/>}</p>
             <p>{item.toDate}</p>
             <p>{item.courseLink}</p>
         </div>
