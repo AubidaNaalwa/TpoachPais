49cc1315c8af3b7aaf36b488cc22b936f3743f1f
Noor Massarwe <noor_19771@outlook.com>
2021-02-21 18:50:35 +0200

    my work

---
+++ src/components/ExperimentTybe.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import Experiments  from './Experiments';
+import '../styles/AubidaStyle.css'
+const axios = require('axios');
+
+export default function ExperimentTybe() {
+  
+    return (
+        <button>فيزياء</button>
+        
+    )
+}
+
