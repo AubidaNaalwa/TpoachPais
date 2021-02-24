b16eb682bcfde15fbc062193bbfb044562e7b45c
Your username <suhaha.git@gmail.com>
2021-02-24 17:35:12 +0200

    edit&delete

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -4,15 +4,43 @@ import Grid from '@material-ui/core/Grid';
 const axios = require('axios');
 
 export default function Courses(props) {
+    
     const [courses, setCourses] = useState([]);
+    const [currPath, setCurrPath] = useState('');
+
+    const handleRemove = (id) => {
+        try {
+            axios.delete(`/course/${currPath}/delete/id=${id}`)
+            const tempCourses = [...courses]
+            const i = tempCourses.findIndex(c => c._id === id)
+            tempCourses.splice(i, 1)
+            setCourses(tempCourses)
+        } catch (error) {
+            console.log(error.toString())
+        }
+    }
+
+    const handleEdit = (course) => {
+        try {
+            axios.put(`/course/${currPath}/update/id=${course._id}`, course)
+            const tempCourses = [...courses]
+            const i = tempCourses.findIndex(c => c._id === course._id)
+            tempCourses[i] = course
+            setCourses(tempCourses)
+        } catch (error) {
+            console.log(error.toString())
+        }
+    }
 
     useEffect(() => {
         async function fetchCourses() {
             let courses
             if (window.location.pathname === '/tpais/courses') {
                 courses = await axios.get(`/courses`);
+                setCurrPath('tpoach')
             } else {
                 courses = await axios.get(`/space/courses`);
+                setCurrPath('space')
             }
             setCourses(courses.data.courses);
         }
@@ -20,11 +48,11 @@ export default function Courses(props) {
     }, []);
 
     return (
-        <Grid container direction="row" justify="space-between" spacing={3} style={{ marginTop: 15 }}>
+        <Grid container direction="row" justify="flex-start" spacing={10} style={{ marginTop: -10, marginRight: -1}}>
             {
                 courses.map((c) => (
                     <Grid item key={c._id}>
-                        <CourseCard setCourse={props.setCourse} course={c} />
+                        <CourseCard setCourse={props.setCourse} handleRemove={handleRemove} handleEdit={handleEdit} course={c} />
                     </Grid>
                 ))
             }
--- src/components/Experiments/Experiments.js
+++ src/components/Experiments/Experiments.js
@@ -4,36 +4,59 @@ import Grid from '@material-ui/core/Grid';
 const axios = require('axios');
 
 export default function Experiments(props) {
-    const [experiments, setExperiments] = useState([]);
 
-    
-        
-        useEffect(() => {
+    const [experiments, setExperiments] = useState([]);
+    const [currPath, setCurrPath] = useState('');
+
+    const handleRemove = (id) => {
+        try {
+            axios.delete(`/experiments/${currPath}/delete/id=${id}`)
+            const tempExperiments = [...experiments]
+            const i = tempExperiments.findIndex(e => e._id === id)
+            tempExperiments.splice(i, 1)
+            setExperiments(tempExperiments)
+        } catch (error) {
+            console.log(error.toString())
+        }
+    }
+
+    const handleEdit = (experiment) => {
+        try {
+            axios.put(`/experiments/${currPath}/update/id=${experiment._id}`, experiment)
+            const tempExperiments = [...experiments]
+            const i = tempExperiments.findIndex(e => e._id === experiment._id)
+            tempExperiments[i] = experiment
+            setExperiments(tempExperiments)
+        } catch (error) {
+            console.log(error.toString())
+        }
+    }
+
+    useEffect(() => {
         async function fetchExperiments() {
-        let experiments
+            let experiments
 
             if (window.location.pathname === '/tpais/experiments') {
                 experiments = await axios.get(`/experiments`);
+                setCurrPath('tpoach')
             } else {
                 experiments = await axios.get(`/space/experiments`);
+                setCurrPath('space')
             }
             setExperiments(experiments.data.experiments);
         }
         fetchExperiments();
     }, []);
 
-    
-
-
     return (
-        <Grid container direction="row" justify="space-between" spacing={3} style={{marginTop: 15}}>
-        {
-            experiments.map((e) => (
-                <Grid item key={e._id}>
-                    <ExperimentCard setExperiment={props.setExperiment} experiment={e} />
-                </Grid>
-            ))
-        }
+        <Grid container direction="row" justify="flex-start" spacing={10} style={{ marginTop: -10, marginRight: -1 }}>
+            {
+                experiments.map((e) => (
+                    <Grid item key={e._id}>
+                        <ExperimentCard setExperiment={props.setExperiment} handleRemove={handleRemove} handleEdit={handleEdit} experiment={e} />
+                    </Grid>
+                ))
+            }
         </Grid>
     );
 }
